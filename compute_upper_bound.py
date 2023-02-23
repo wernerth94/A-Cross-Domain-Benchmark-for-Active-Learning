9c41ca377eff1ab7752c7669d20072e024626983
Thorben Werner <werner@ismll.de>
2023-02-23 12:57:43 +0100

    data folder rework

--- compute_upper_bound.py
+++ compute_upper_bound.py
@@ -5,36 +5,39 @@ from core.classifier import fit_and_evaluate
 from core.helper_functions import *
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--data_folder", type=str, required=True)
 parser.add_argument("--run_id", type=int, default=1)
 parser.add_argument("--dataset", type=str, default="splice")
-parser.add_argument("--restarts", type=int, default=5) # TODO
+parser.add_argument("--restarts", type=int, default=15)
 args = parser.parse_args()
 
-numpy.random.seed(args.run_id)
-torch.random.manual_seed(args.run_id)
+run_id = args.run_id
+max_run_id = run_id + args.restarts
+while run_id < max_run_id:
+    numpy.random.seed(run_id)
+    torch.random.manual_seed(run_id)
 
-DatasetClass = get_dataset_by_name(args.dataset)
-dataset = DatasetClass(cache_folder="../datasets")
-dataset = dataset.to(util.device)
+    DatasetClass = get_dataset_by_name(args.dataset)
+    dataset = DatasetClass(cache_folder=args.data_folder)
+    dataset = dataset.to(util.device)
 
-base_path = os.path.join("runs", dataset.name, "UpperBound")
-log_path = os.path.join(base_path, f"run_{args.run_id}")
+    base_path = os.path.join("runs", dataset.name, "UpperBound")
+    log_path = os.path.join(base_path, f"run_{run_id}")
 
-save_meta_data(log_path, None, None, dataset)
+    save_meta_data(log_path, None, None, dataset)
 
-accuracies = []
-for _ in range(args.restarts):
-    accs = fit_and_evaluate(dataset)
-    accuracies.append(accs)
-# save final accuracies for performance comparisons
-data_dict = {}
-for k, v in enumerate(accuracies):
-    data_dict[k] = [v[-1]]
-df = pd.DataFrame(data_dict)
-df.to_csv(os.path.join(log_path, "accuracies.csv"))
+    # some convoluted saving to make it compatible with collect_results
+    # and to be consistent with logging of other runs
+    accuracies = [fit_and_evaluate(dataset)]
+    data_dict = {}
+    for k, v in enumerate(accuracies):
+        data_dict[k] = [v[-1]]
+    df = pd.DataFrame(data_dict)
+    df.to_csv(os.path.join(log_path, "accuracies.csv"))
 
-# save learning curves for training evaluation
-plot_learning_curves(accuracies, os.path.join(log_path, "learning_curves.jpg"))
+    # save learning curves for training evaluation
+    plot_learning_curves(accuracies, os.path.join(log_path, "learning_curves.jpg"))
 
-# collect results from all runs
-collect_results(base_path, "run_")
+    # collect results from all runs
+    collect_results(base_path, "run_")
+    run_id += 1
--- evaluate_oracle.py
+++ evaluate_oracle.py
@@ -1,40 +1,43 @@
 import experiment_util as util
-import os, argparse
-from time import time
-import torch
+import argparse
 import numpy
 from tqdm import tqdm
-import datasets
 import core
 from core.helper_functions import *
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--data_folder", type=str, required=True)
 parser.add_argument("--run_id", type=int, default=1)
 parser.add_argument("--dataset", type=str, default="splice")
 parser.add_argument("--sample_size", type=int, default=20)
 parser.add_argument("--restarts", type=int, default=50)
 args = parser.parse_args()
 
-numpy.random.seed(args.run_id)
-torch.random.manual_seed(args.run_id)
+run_id = args.run_id
+max_run_id = run_id + args.restarts
+while run_id < max_run_id:
+    numpy.random.seed(run_id)
+    torch.random.manual_seed(run_id)
 
-DatasetClass = get_dataset_by_name(args.dataset)
-dataset = DatasetClass(cache_folder="../datasets")
-dataset = dataset.to(util.device)
-env = core.OracleALGame(dataset,
-                        args.sample_size,
-                        device=util.device)
-base_path = os.path.join("runs", dataset.name, f"Oracle")
-log_path = os.path.join(base_path, f"run_{args.run_id}")
-save_meta_data(log_path, None, env, dataset)
+    DatasetClass = get_dataset_by_name(args.dataset)
+    dataset = DatasetClass(cache_folder=args.data_folder)
+    dataset = dataset.to(util.device)
+    env = core.OracleALGame(dataset,
+                            args.sample_size,
+                            device=util.device)
+    base_path = os.path.join("runs", dataset.name, f"Oracle")
+    log_path = os.path.join(base_path, f"run_{run_id}")
+    save_meta_data(log_path, None, env, dataset)
 
-with core.EnvironmentLogger(env, log_path, util.is_cluster, args.restarts) as env:
-    for _ in tqdm(range(args.restarts)):
+    with core.EnvironmentLogger(env, log_path, util.is_cluster) as env:
         done = False
         dataset.reset()
         state = env.reset()
-        while not done:
+        for i in tqdm(range(env.env.budget)):
             state, reward, done, truncated, info = env.step()
+            if done or truncated:
+                break # fail save; should not happen
 
-# collect results from all runs
-collect_results(base_path, "run_")
+    # collect results from all runs
+    collect_results(base_path, "run_")
+    run_id += 1
