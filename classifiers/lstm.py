ce77836af36ca904ed60726e7c0ad201dc8065cd
Thorben Werner <werner@ismll.de>
2023-05-12 14:48:18 +0200

    lstm model gpu fix

--- classifiers/lstm.py
+++ classifiers/lstm.py
@@ -29,10 +29,11 @@ class BiLSTMModel(nn.Module):
         device = next(self.parameters()).device
         # Count non-zero embeddings
         with torch.no_grad():
-            num_of_pad = (x == self.pad_idx).int().sum(dim=-1).to(device)
+            num_of_pad = (x == self.pad_idx).int().sum(dim=-1)
             lens = torch.ones(len(x)) * x.size(-1)
-            lens -= num_of_pad
+            num_of_pad = num_of_pad.to(device)
             lens = lens.to(device)
+            lens -= num_of_pad
         x = self.word_embedding(x)
         embeddings_pack = pack_padded_sequence(x, lens,
                                                batch_first=True, enforce_sorted=False)
