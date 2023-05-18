06376739a2ffcaa900607f881015554685d19934
Thorben Werner <werner@ismll.de>
2023-05-18 16:52:55 +0200

    finishing the runs

--- results/generate_result_table.py
+++ results/generate_result_table.py
@@ -6,7 +6,7 @@ import numpy as np
 def generate_table(agents, datasets, out_file):
     result_table = pd.DataFrame(index=agents, columns=datasets)
     for dataset in datasets:
-        dataset_folder = os.path.join("../runs", dataset)
+        dataset_folder = os.path.join("runs", dataset)
         if not os.path.isdir(dataset_folder):
             continue
         agent_aucs = dict()
@@ -22,17 +22,21 @@ def generate_table(agents, datasets, out_file):
                     values = accuracies.values
                     auc = np.sum(values, axis=0) / values.shape[0]
                     result_table[dataset][agent] = "%1.3f +- %1.2f"%(np.mean(auc).item(), np.std(auc).item())
+            else:
+                print(f"{agent} is missing for {dataset}")
 
     if len(result_table) > 0:
-        result_table_file = os.path.join("../runs", out_file)
+        result_table_file = os.path.join("runs", out_file)
         if os.path.exists(result_table_file):
             os.remove(result_table_file)
         result_table.to_csv(result_table_file)
 
 
-agents = ["Oracle", "SAL_23_01_16", "Coreset_Greedy", "MarginScore", "ShannonEntropy", "RandomAgent"]
+agents = ["Oracle", "SAL_23_01_16", "Coreset_Greedy", "TypiClust", "MarginScore", "ShannonEntropy", "RandomAgent", "Badge", "BALD", ]
 vector_data = ["Splice", "DNA", "USPS"]
 img_data = ["Cifar10Encoded", "FashionMnistEncoded"]
+text_data = ["TopV2", "News"]
 
 generate_table(agents, vector_data, "result_vector.csv")
 generate_table(agents, img_data, "result_img.csv")
+generate_table(agents, text_data, "result_text.csv")
