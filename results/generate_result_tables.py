82197f68bccccd907bd9b8d25593584b0265a736
Thorben Werner <werner@ismll.de>
2023-05-31 17:46:30 +0200

    paper update

--- results/generate_result_tables.py
+++ results/generate_result_tables.py
@@ -3,17 +3,9 @@ from os.path import exists, join
 import pandas as pd
 import numpy as np
 
-def _pad_nans_with_last_value(df:pd.DataFrame):
-    max_len = len(df)
-    for col in df:
-        diff = max_len - sum(pd.notna(df[col]))
-        if diff > 0:
-            last_val = df[col][sum(pd.notna(df[col])) - 1]
-            df[col] = pd.concat([df[col].iloc[:-diff], pd.Series([last_val]*diff)], ignore_index=True)
-    return df
-
 def generate_table(agents, datasets, out_file):
     result_table = pd.DataFrame(index=agents, columns=datasets)
+    result_table["Sum"] = [0.0]*len(agents)
     for dataset in datasets:
         dataset_folder = os.path.join("runs", dataset)
         if not os.path.isdir(dataset_folder):
@@ -28,21 +20,24 @@ def generate_table(agents, datasets, out_file):
                 acc_file = join(agent_folder, "accuracies.csv")
                 if exists(acc_file):
                     accuracies = pd.read_csv(acc_file, header=0, index_col=0)
-                    accuracies = _pad_nans_with_last_value(accuracies)
                     values = accuracies.values
                     auc = np.sum(values, axis=0) / values.shape[0]
-                    result_table[dataset][agent] = "%1.3f +- %1.2f"%(np.mean(auc).item(), np.std(auc).item())
+                    result_table.loc[agent, "Sum"] += np.median(auc).item()
+                    result_table.loc[agent, dataset] = "%1.3f +- %1.2f"%(np.median(auc).item(), np.std(auc).item())
             else:
                 print(f"{agent} is missing for {dataset}")
 
     if len(result_table) > 0:
+        result_table = result_table.sort_values("Sum", ascending=False)
+        result_table = result_table.drop(columns=["Sum"])
+
         result_table_file = os.path.join("results", out_file)
         if os.path.exists(result_table_file):
             os.remove(result_table_file)
         result_table.to_csv(result_table_file)
 
 
-agents = ["Oracle", "SAL_23_01_16", "Coreset_Greedy", "TypiClust", "MarginScore", "ShannonEntropy", "RandomAgent", "Badge", "BALD", ]
+agents = ["Oracle", "Coreset_Greedy", "TypiClust", "MarginScore", "ShannonEntropy", "RandomAgent", "Badge", "BALD", ]
 vector_data = ["Splice", "DNA", "USPS"]
 enc_vector_data = ["SpliceEncoded", "DNAEncoded", "USPSEncoded"]
 img_data = ["Cifar10Encoded", "FashionMnistEncoded"]
